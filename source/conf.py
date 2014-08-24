72fc0f0da7b3a5b6e5df29abcd3860d17410298f
Philipp Heckel <philipp.heckel@gmail.com>
2014-08-24 04:49:24 +0200

    RTD compile issue

--- source/conf.py
+++ source/conf.py
@@ -132,7 +132,8 @@ html_static_path = ['_static']
 
 html_context = {
     'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
+        '_static/theme.css', 
+        '_static/theme_overrides.css',
         ],
     }
 
